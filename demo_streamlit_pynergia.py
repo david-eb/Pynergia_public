8b7eac5788a6b7281bbe1689cd0fe616c9739dea
david-eb <83336637+david-eb@users.noreply.github.com>
2021-09-08 16:49:59 +0200

    Update demo_streamlit_pynergia.py

--- demo_streamlit_pynergia.py
+++ demo_streamlit_pynergia.py
@@ -10,7 +10,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 
 from sklearn.metrics import mean_squared_error
 import tensorflow as tf
